27279a37a53a9126767063951888c148bcbc2bf5
Martin Etmajer <m.etmajer@gmail.com>
2015-08-26 16:26:42 +0200

    Clean up.

--- dtcollector-with-config/run.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-mkdir -p /tmp/dynatrace/dtcollector-with-config/log
-sudo docker run --hostname="dtcollector-with-config" -p 9993:9998 --name=dtcollector-with-config -d -v /tmp/dynatrace/dtcollector-with-config/log:/dynatrace/log dynatrace/dtcollector-with-config "$@" && \
-echo Started configured dynaTrace Collector, exposing Agent connections on port 9993
