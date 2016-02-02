c88c8b003d996ad8b8bc69efbd0e9c3d1aed712d
Martin Etmajer <m.etmajer@gmail.com>
2016-02-02 23:17:26 +0100

    Reduce Java Agent example.

--- Dynatrace-Agent-Examples/java/build/JavaTest.java
+++ Dynatrace-Agent-Examples/java/build/JavaTest.java
@@ -3,15 +3,7 @@ public class JavaTest {
 	public static void main(String args[]) {
 		try {
 			while (true) {
-				try {
-					throw new Exception();
-				}
-				catch (Exception e) {
-					System.out.println("Threw Exception...");
-				}
-				finally {
-					Thread.sleep(1000);
-				}
+				Thread.sleep(1000);
 			}
 		}
 		catch (java.lang.InterruptedException e) {
