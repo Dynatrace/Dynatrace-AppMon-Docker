74208e70b18a374e7b4ed66e22df9dc5a43806b4
Martin Etmajer <m.etmajer@gmail.com>
2016-02-03 20:23:41 +0100

    Fix name of shell script.

--- Dynatrace-WebServer-Agent-Examples/nginx/run-container.sh
+++ Dynatrace-WebServer-Agent-Examples/nginx/run-container.sh
@@ -7,5 +7,5 @@ docker run --rm \
   --ipc container:dtwsagent \
   --publish-all \
   nginx \
-  sh -c "(sleep 1 && \${DTWSAGENT_ENV_DT}/attach-wsagent-slave.sh &) && \
+  sh -c "(sleep 1 && \${DTWSAGENT_ENV_DT}/attach-to-wsagent-master.sh &) && \
          LD_PRELOAD=\${DTWSAGENT_ENV_LIB64} nginx"
