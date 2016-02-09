b86793a3bd94436fc27fb6f11e2663b687395389
Martin Etmajer <m.etmajer@gmail.com>
2016-02-09 16:38:36 +0100

    Move 'socat' into the Dynatrace directory. Allow override of the master agent location in 'attach-to-wsagent-master.sh' via WSAGENT.

--- Dynatrace-WebServer-Agent/build/accept-wsagent-slaves.sh
+++ Dynatrace-WebServer-Agent/build/accept-wsagent-slaves.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # socat forwards incoming slaves to the loopback network interface.
-${SOCAT_HOME}/socat udp4-listen:${SLAVE_AGENT_PORT},fork udp:localhost:${SLAVE_AGENT_PORT}
+${DT}/socat udp4-listen:${SLAVE_AGENT_PORT},fork udp:localhost:${SLAVE_AGENT_PORT}
--- Dynatrace-WebServer-Agent/build/attach-to-wsagent-master.sh
+++ Dynatrace-WebServer-Agent/build/attach-to-wsagent-master.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+DT=`find / -name dynatrace`
+
+# We attempt to auto-discover the Dynatrace Web Server (master agent) through
+# the environment when the container has been --linked to a 'dynatrace/wsagent'
+# container instance with a link alias 'dtwsagent'.
+#
+# Example: docker run --link dtwsagent-1:dtwsagent httpd
+#
+# Auto-discovery can be overridden by providing the $WSAGENT variable
+# through the environment.
+WSAGENT_SLAVE_AGENT_PORT=`grep -E '^#?Port ' ${DT}/agent/conf/dtwsagent.ini | cut -d' ' -f2`
+
+WSAGENT_HOST_NAME=${DTWSAGENT_ENV_HOST_NAME:-"docker-dtwsagent"}
+WSAGENT_SLAVE_AGENT_PORT=${DTWSAGENT_ENV_SLAVE_AGENT_PORT:-"${WSAGENT_SLAVE_AGENT_PORT}"}
+WSAGENT=${WSAGENT:-"${WSAGENT_HOST_NAME}:${WSAGENT_SLAVE_AGENT_PORT}"}
 
 # socat forwards incoming slaves to the dtwsagent's host.
-${DTWSAGENT_ENV_SOCAT_HOME}/socat udp-listen:${DTWSAGENT_ENV_SLAVE_AGENT_PORT},fork udp:${DTWSAGENT_ENV_HOST_NAME}:${DTWSAGENT_ENV_SLAVE_AGENT_PORT}
+${DT}/socat udp-listen:`echo ${WSAGENT} | cut -d':' -f2`,fork udp:${WSAGENT}
