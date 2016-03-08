aaf2b0c7a2f4bebad6d23dffce0f2795b5f6cc82
Martin Etmajer <m.etmajer@gmail.com>
2016-03-08 14:43:47 +0100

    Make linking to a dtagent container entirely optional.

--- Dynatrace-Agent/build/scripts/run-wsagent.sh
+++ Dynatrace-Agent/build/scripts/run-wsagent.sh
@@ -14,6 +14,19 @@ COLLECTOR_HOST_NAME=${DTCOLLECTOR_ENV_HOST_NAME:-"docker-dtcollector"}
 COLLECTOR_PORT=${DTCOLLECTOR_ENV_AGENT_PORT:-"9998"}
 COLLECTOR=${DT_AGENT_COLLECTOR:-"${COLLECTOR_HOST_NAME}:${COLLECTOR_PORT}"}
 
+# Attempt to auto-discover the Dynatrace Web Server Agent binary through
+# the environment when the container has been --linked to a 'dynatrace/agent'
+# container instance with a link alias 'dtagent'.
+#
+# Example: docker run --link dtagent-1:dtagent httpd
+#
+# Auto-discovery can be overridden by providing the $BIN variable through
+# the environment, or as the first argument to this script.
+BIN=${1:-"${DTAGENT_ENV_BIN64}"}
+if [ -z ${BIN} ]; then
+  BIN=/dynatrace/agent/lib64/dtwsagent
+fi
+
 # Attempt to auto-discover the Dynatrace Web Server Agent configuration through
 # the environment when the container has been --linked to a 'dynatrace/agent'
 # container instance with a link alias 'dtagent'.
@@ -21,9 +34,12 @@ COLLECTOR=${DT_AGENT_COLLECTOR:-"${COLLECTOR_HOST_NAME}:${COLLECTOR_PORT}"}
 # Example: docker run --link dtagent-1:dtagent httpd
 #
 # Auto-discovery can be overridden by providing the $INI variable through
-# the environment.
-INI=${DTAGENT_ENV_INI:-"/dynatrace/agent/conf/dtwsagent.ini"}
+# the environment, or as the second argument to this script.
+INI=${2:-"${DTAGENT_ENV_INI}"}
+if [ -z ${INI} ]; then
+  INI=/dynatrace/agent/conf/dtwsagent.ini
+fi
 
 sed -i -r "s/^#?Name.*/Name ${AGENT_NAME}/;s/^#?Server.*/Server ${COLLECTOR}/;s/^#?Loglevel.*/Loglevel ${LOG_LEVEL}/;s/^#?ConsoleLoglevel.*/ConsoleLoglevel ${LOG_LEVEL}/" ${INI}
 
-${DTAGENT_ENV_BIN64} &
+${BIN} &
