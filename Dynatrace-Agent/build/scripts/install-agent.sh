cbace01e7322ed7113e92e24386902c1d30da2c8
BlazejT <blazej.tomaszewski@dynatrace.com>
2017-10-25 12:11:26 +0000

    Make curl insecure only as an option

--- Dynatrace-Agent/build/scripts/install-agent.sh
+++ Dynatrace-Agent/build/scripts/install-agent.sh
@@ -3,7 +3,7 @@ INSTALLER_URL="$1"
 INSTALLER_FILE_NAME=`basename ${INSTALLER_URL}`
 
 cd /tmp
-curl --insecure -L -O ${INSTALLER_URL}
+curl ${CURL_INSECURE:+"--insecure"} -L -O ${INSTALLER_URL}
 java -jar ./${INSTALLER_FILE_NAME} -y
 cp -R ./dynatrace-${VERSION}/* ${DT_HOME}
 rm -rf /tmp/*
--- Dynatrace-Agent/build/scripts/install-node-agent.sh
+++ Dynatrace-Agent/build/scripts/install-node-agent.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-INSTALLER_URL="$1"
+BINSTALLER_URL="$1"
 INSTALLER_FILE_NAME=`basename ${INSTALLER_URL}`
 
 cd /tmp
-curl --insecure -L -O ${INSTALLER_URL}
+curl ${CURL_INSECURE:+"--insecure"} -L -O ${INSTALLER_URL}
 tar -xvzf ./${INSTALLER_FILE_NAME}
 cp -R ./dynatrace-oneagent-${VERSION}/* ${DT_HOME}
 rm -rf /tmp/*
